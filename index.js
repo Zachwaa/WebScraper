34c79f87b9bff45b920d2c76903648449d69ea1a
Zachary Tonge <119691687+Zachwaa@users.noreply.github.com>
2023-09-22 22:05:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
-const Knwl = require('knwl.js')
 const isURLhttp = require('is-url-http')
-
-const newknwl = new Knwl("english")
+const Knwl = require('knwl.js')
+const knwlInstance = new Knwl()
+knwlInstance.register("phone",require('knwl.js/default_plugins/phones'))
+knwlInstance.register("address",require('knwl.js/default_plugins/places'))
 
 const examples = [
-    "info@1stchoiceframes.co.uk", //Does not return link, only contact.html
-    "info@workwearoutlet.co.uk",  //Works but returns 3 of the same
-    "info@powertooldirect.co.uk", //Works
-    "info@onesvs.co.uk",  //Partially works. Return 2 correct and 1 false
-    "info@easyprintltd.co.uk", //returns telephone number as URL
-    "info@shethsinteriors.co.uk",  // returns #contact
+    "info@1stchoiceframes.co.uk", //only returns address
+    "info@workwearoutlet.co.uk",  //
+    "info@powertooldirect.co.uk", //returns phone number
+    "info@onesvs.co.uk",  //returns phone nummber
+    "info@easyprintltd.co.uk", //returns address
+    "info@shethsinteriors.co.uk",  // 
     "info@northernpropertycontractors.co.uk", 
-    "info@vital-life.co.uk", // Returns directory (multiple)
-    "info@zoomrecruitment.co.uk", // Returns directory (multiple)
-    "info@blackburnyz.org", //Works but returns 4 of the same
-    "info@nightsafe.org", //Works 
-    "info@cumminsmellor.co.uk", //Returns Directory (2)
+    "info@vital-life.co.uk", // returns phone number
+    "info@zoomrecruitment.co.uk", // returns both
+    "info@blackburnyz.org", //
+    "info@nightsafe.org", //Returns many phone numbers
+    "info@cumminsmellor.co.uk", //returns phone number
 ]
 
-UsefulInfo = []
+var UsefulInfo = []
 let strippedArrays = []
 
 //Converts each email into a readable domain
@@ -31,12 +32,12 @@ examples.forEach((ele) => {
 
 async function ScrapeWebsite(){
 
-    //Loads Website
     for (i=0;i<strippedArrays.length;i++){
         try {
+            const currentWebsite = strippedArrays[i]
             const response = await axios.request({
                 method: "GET",
-                url: strippedArrays[i],
+                url: currentWebsite,
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
                 }
@@ -44,74 +45,140 @@ async function ScrapeWebsite(){
             const $ = cheerio.load(response.data)
             var contactURL = ""
    
-            $("a").each((index,link) => {    //Could search footer and header only
-                var text = $(link).text().toLowerCase()
-            
-                // Check if text of every link contains 'contact' 
-                if (text.includes("contact")){
-                   // UsefulInfo.push({text: text, website: strippedArrays[i]})
-                    contactURL =  $(link).attr("href")
-                    return false
-                } 
+           $("a, span").each((index,val) => {
+                if ($(val).attr("href")){
+                    if ($(val).text().toLowerCase().includes("contact")){
+                        contactURL = $(val).attr("href")
+                        return false
+                    }
+                }
             }) 
 
             while (contactURL[0] == "/"){
                 contactURL = contactURL.substring(1) //Makes sure there is only 1 / 
             }
 
-            properURL = strippedArrays[i]
-
+            validURL = currentWebsite
             if (contactURL){
-
                 if (isURLhttp(contactURL)){
-                    //Go straight to contact page using contactURL
-                    properURL = contactURL
-
-                } else if (isURLhttp(strippedArrays[i] + contactURL) && (contactURL.toLowerCase().includes("contact"))){
-                    properURL = strippedArrays[i] + contactURL
-                    //If contact is found but is only a directory link 
+                    validURL = contactURL
+                } else if (isURLhttp(currentWebsite + contactURL) && (contactURL.toLowerCase().includes("contact"))){
+                    validURL = strippedArrays[i] + contactURL
                 }
             }
-           // UsefulInfo.push({URL : properURL, website : strippedArrays[i]  ,isURL: true})
 
             const contactResponse = await axios.request({
                 method: "GET",
-                url: properURL,
+                url: validURL,
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
                 }
             })
 
+
+
             const $$ = cheerio.load(contactResponse.data)
-            
-            const PhoneNumberRegex = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
-            const AddressRegex = /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/
+            const PhoneNumberRegex =  /^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$/  // /^(\+{1}\d{2,3}\s?[(]{1}\d{1,3}[)]{1}\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}$/
+           // const AddressRegex = /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
 
-            contactInfo = {}
-            
-            $$('*').each((index,ele) => {
-                let NewEle = $(ele).text()
-                if (PhoneNumberRegex.test(NewEle)){
-                    contactInfo.PhoneNumber = NewEle
+            var Numbers = []
+            var Locations = []
+
+         /*   $$('body *').filter(function () {
+                 knwlInstance.init($(this).text())
+                var phoneNumber = knwlInstance.get("phone")
+                var address =  knwlInstance.get("address")
+                address.forEach((address) => {
+                    Locations.push(address.place)
+                })  
+
+                phoneNumber.forEach((number) => {
+                    Numbers.push(number.phone)
+                }) 
+                var text = $$(this).text()
+                if ($$(this).attr("href")){
+                    if (($$(this).attr("href")).replace(/\D/g,'').search(PhoneNumberRegex) !== -1){
+                        return true
+                    }
                 }
-                if (AddressRegex.test(NewEle)){
-                    contactInfo.AddressRegex = NewEle
+                if (text == ''){
+                    return false
                 }
-            })
+                var justNumbers = text.replace(/\D/g,'')
+                if (justNumbers )
 
-            contactInfo.contact = properURL
+                return (justNumbers).search(PhoneNumberRegex) !== -1     //   PhoneNumberRegex.test($(this).text());
+                })
+                .each(function () {
+                    var obj = $$(this)
+                    if (obj.attr("href")) {
+                        Numbers.push(obj.attr("href").replace(/\D/g,' '));
+                        
+                    }
 
-            UsefulInfo.push(contactInfo)
+                    noOfNumbers= obj.text().split(/\s+/)
+                    noOfNumbers.forEach((val) => {
+                        Numbers.push(val)
+                    })
+                    
+                    Numbers.push(obj.text().replace(/\D/g,''));
+
+                    
+                    
+                }); */
+                $$('body *').each(function () {
+                    var obj = $$(this)
+                    var text = obj.text()
+                    if (text !== ''){
+                        var justNumbers = text.replace(/\D/g,'')
+                        var splitNumbers = text.replace(/\D/g,' ').split(/\s{2,}/)
+    
+                      /*  splitNumbers.forEach((val) => {
+                            if (val.replace(/\D/g,'').search(PhoneNumberRegex) !== -1){
+                                Numbers.push(val.replace(/\D/g,''))
+                            }
+                        }) */
+
+                        if ((justNumbers).search(PhoneNumberRegex) !== -1 ){
+                            Numbers.push(justNumbers);
+                        } else {
+                            text.replace(/\D/g,' ').split(/\s{2,}/).forEach((val) => {
+                                if (val.replace(/\D/g,'').search(PhoneNumberRegex) !== -1){
+                                    Numbers.push(val.replace(/\D/g,''))
+                                }
+                            })
+                        }
+                    }
+                    if (obj.attr("href")){
+                        if ((obj.attr("href")).replace(/\D/g,'').search(PhoneNumberRegex) !== -1){
+                            Numbers.push(obj.attr("href").replace(/\D/g,' '))
+                        }
+                    }
+                  
+                /*    if (text !== ''){
+                        if ((justNumbers).search(PhoneNumberRegex) !== -1 ){
+                            Numbers.push(justNumbers);
+                        } else if{
+
+                        } 
+
+                    } */
+                })
+            
+
+
+
+            UsefulInfo.push({potentialNumbers : Numbers, potentialLocal: Locations, contact: validURL}) 
 
         } catch(error) {
             console.log(error)
             return
-        }   
+        } 
+
+      
     }
 
     console.log(UsefulInfo)
 }
 
 ScrapeWebsite()
-
-// Does not consider the link may be li or span element
